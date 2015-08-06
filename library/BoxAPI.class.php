15c0de70da69006ef43e3b016a1f3bcc54fa9a7f
Ulhas Vardhan Golchha <golchha21@gmail.com>
2015-08-06 14:59:06 +0530

    Version updated

--- library/BoxAPI.class.php
+++ library/BoxAPI.class.php
@@ -1,6 +1,6 @@
 <?php 
 	define( '_CODENAME', 'BoxPHPAPI'); 
-	define( '_VERSION', '1.0.6'); 
+	define( '_VERSION', '1.0.7'); 
 	define( '_URL', 'https://github.com/golchha21/BoxPHPAPI');
 	error_reporting(E_ERROR);
 	
