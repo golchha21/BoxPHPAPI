39b17525163c51278bef90c1048a36f7d6d0093f
misterfifi1 <misterfifi1@users.noreply.github.com>
2015-08-21 10:57:10 +0200

    Update BoxAPI.class.php

--- library/BoxAPI.class.php
+++ library/BoxAPI.class.php
@@ -1,7 +1,7 @@
 <?php 
 	define( '_CODENAME', 'BoxPHPAPI'); 
 	define( '_VERSION', '1.0.6'); 
-	define( '_URL', 'https://github.com/mitserfifi1/BoxPHPAPI');
+	define( '_URL', 'https://github.com/misterfifi1/BoxPHPAPI');
 	define( '_ENTERPRISE_ID', '371568');
 	error_reporting(E_ERROR);
 	
